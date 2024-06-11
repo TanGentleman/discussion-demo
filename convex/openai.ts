ee9dc910ae5d1b2d6e13824b7ea8a206cf352948
Tanuj <Tanuj@cmu.edu>
2024-06-11 15:45:16 -0700

    Support TogetherAI and buffered streaming

--- convex/openai.ts
+++ convex/openai.ts
@@ -3,6 +3,17 @@ import { internalAction } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 import { internal } from "./_generated/api";
 
+const overrideBaseUrl = "https://api.together.xyz/v1";
+const overrideModelName = "cognitivecomputations/dolphin-2.5-mixtral-8x7b";
+
+// How often to update the DB to stream the response back to the client
+// Recommended is 200, but it is smoother at lower values
+const chunkCacheSize = 50;
+
+// const allowedBaseUrls = ["https://api.openai.com/v1", "https://api.together.xyz/v1"];
+// const allowedModelNames = ["Snowflake/snowflake-arctic-instruct", "Qwen/Qwen2-72B-Instruct", "cognitivecomputations/dolphin-2.5-mixtral-8x7b"];
+
+
 type ChatParams = {
   messages: Doc<"messages">[];
   messageId: Id<"messages">;
@@ -10,11 +21,14 @@ type ChatParams = {
 export const chat = internalAction({
   handler: async (ctx, { messages, messageId }: ChatParams) => {
     const apiKey = process.env.OPENAI_API_KEY!;
-    const openai = new OpenAI({ apiKey });
+    const baseURL = overrideBaseUrl;
+    const openai = new OpenAI({ baseURL, apiKey });
 
     try {
       const stream = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo", // "gpt-4" also works, but is so slow!
+        // model: "gpt-3.5-turbo", // "gpt-4" also works, but is so slow!
+        // Changed June 11 2024: Added TogetherAI as a provider
+        model: overrideModelName,
         stream: true,
         messages: [
           {
@@ -29,16 +43,30 @@ export const chat = internalAction({
         ],
       });
       let body = "";
+      let partSize = 0;
       for await (const part of stream) {
         if (part.choices[0].delta?.content) {
-          body += part.choices[0].delta.content;
-          // Alternatively you could wait for complete words / sentences.
-          // Here we send an update on every stream message.
-          await ctx.runMutation(internal.messages.update, {
-            messageId,
-            body,
-          });
+          const partContent = part.choices[0].delta.content;
+          body += partContent;
+          partSize += partContent.length;
+          if (partSize > chunkCacheSize) {
+            // Send an update to the client
+            await ctx.runMutation(internal.messages.update, {
+              messageId,
+              body,
+            });
+            partSize = 0;
+          }
         }
+      }
+      // If full stream has not been flushed yet
+      if (partSize > 0) {
+        // Send a last update to the client
+        await ctx.runMutation(internal.messages.update, {
+          messageId,
+          body,
+        });
+        partSize = 0;
       }
     } catch (e) {
       if (e instanceof OpenAI.APIError) {
