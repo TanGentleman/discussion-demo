0b491bc10b448516e513935c57621e17e83d3732
Tanuj <Tanuj@cmu.edu>
2024-06-17 22:11:21 -0700

    Create secretConfig.ts

---
+++ convex/secretConfig.ts
@@ -0,0 +1,40 @@
+// Sensitive API keys should be stored in the environment variables using `npx convex env`
+const togetherSecretKey = "Experimental TogetherAI key"
+const openAISecretKey = "Experimental OpenAI key"
+
+const togetherBaseURL = "https://api.together.xyz/v1"
+const togetherModelName = "Qwen/Qwen2-72B-Instruct"
+
+const lmstudioBaseURL = "http://localhost:1234/v1"
+const ollamaBaseURL = "http://localhost:11434/v1"
+
+interface secretConfigSchema {
+    baseURL?: string;
+    modelName: string;
+    apiKey: string;
+}
+
+const lmstudioConfig: secretConfigSchema = {
+    baseURL: lmstudioBaseURL,
+    modelName: "local-model",
+    apiKey: "lm-studio"
+};
+
+const ollamaConfig: secretConfigSchema = {
+    baseURL: ollamaBaseURL,
+    modelName: "llama3",
+    apiKey: "Ollama"
+};
+
+const openAIConfig: secretConfigSchema = {
+    modelName: "gpt-3.5-turbo",
+    apiKey: openAISecretKey
+};
+
+const secretConfig: secretConfigSchema = {
+    baseURL: togetherBaseURL,
+    modelName: togetherModelName,
+    apiKey: togetherSecretKey
+};
+
+export default ollamaConfig;
